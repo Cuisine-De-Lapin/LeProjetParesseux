50ea670a3821d3f52377444a09c258266a451062
Cuisine De Lapin <cuisine_de_lapin@naver.com>
2022-10-31 09:11:34 +0900

    newSingleThreadContext 대신 runBlocking을 사용하는 것으로 수정
    newSingleThreadContext는 스레드를 직접 정해주는 대신 나중에 별도로 클로즈 해야 후환이 없다.
    runblocking은 스레드를 막아줘서 fifo처럼 할 수 있다.
    
    솔직히 스레드에 대해서 fifo라기 보다는
    블록체인에 대해서 fifo를 하고 싶은데
    이건 좀 더 배우면 답이 나오겠지?

--- BlockChain/src/main/java/cuisine/de/lapin/library/blockchain/impl/BlockChainImpl.kt
+++ BlockChain/src/main/java/cuisine/de/lapin/library/blockchain/impl/BlockChainImpl.kt
@@ -8,24 +8,24 @@ import cuisine.de.lapin.library.blockchain.utils.sha256
 import cuisine.de.lapin.library.blockchain.utils.toBlock
 import cuisine.de.lapin.library.blockchain.utils.toJson
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 
 internal class BlockChainImpl(
     private var difficulty: Int,
-    private val timeStamp: Long = System.currentTimeMillis(),
-    threadName: String? = null
+    private val timeStamp: Long = System.currentTimeMillis()
 ) : BlockChain {
     companion object {
         private const val GENESIS_EVENT = "GenesisEvent"
-        private const val DEFAULT_THREAD_NAME = "DefaultBlockChainThread"
     }
 
     private val _blocks = HashMap<String, String>()
     private var _lastestBlockHash: String = ""
     private var _height: UInt = 0u
-    private val coroutineContext = newSingleThreadContext(threadName ?: DEFAULT_THREAD_NAME) // 블록체인 전용 스레드
+    private val coroutineContext = Dispatchers.IO // newSingleThreadContext을 쓰려고 했는데, 쓰레드를 close하기가 마땅치 않아서 runblocking으로 변경
 
     val blocks: Map<String, String> = _blocks
     val lastestBlockHash
@@ -37,14 +37,14 @@ internal class BlockChainImpl(
 
     init {
         CoroutineScope(coroutineContext).launch {
-            withContext(coroutineContext) {
+            runBlocking(coroutineContext) {
                 addBlock(GENESIS_EVENT, timeStamp)
             }
         }
     }
 
     override suspend fun addBlock(content: Any, timeStamp: Long) {
-        withContext(coroutineContext) {
+        runBlocking(coroutineContext) {
             createBlock(content, _lastestBlockHash, ++_height, timeStamp, difficulty)
                 .let { block ->
                     _height = block.height
@@ -59,28 +59,28 @@ internal class BlockChainImpl(
         this.difficulty = difficulty
     }
 
-    override suspend fun isValidChain(): Boolean = withContext(coroutineContext) {
+    override suspend fun isValidChain(): Boolean = runBlocking(coroutineContext) {
         var currentBlockHash: String? = _lastestBlockHash
         var currentHeight: UInt = _height
         while (true) {
             val block = _blocks[currentBlockHash]?.toBlock() ?: break
             if (--currentHeight == 0u) {
-                return@withContext true
+                return@runBlocking true
             }
 
             currentBlockHash = block.previousHash
         }
 
-        return@withContext false
+        return@runBlocking false
     }
 
-    override suspend fun getBlockChainAsList(): List<Block> = withContext(coroutineContext) {
+    override suspend fun getBlockChainAsList(): List<Block> = runBlocking(coroutineContext) {
         val resultArray = ArrayList<Block>()
 
         var currentBlockHash: String = _lastestBlockHash
         while (true) {
             //prevHash를 받아서 조회를 했는데, 블록이 없는경우는 끊어진 블록이라 유효하지 않은 블록으로 간주함 - 이건 어떻게 처리할지는 고민 필요.
-            val block = getBlock(currentBlockHash) ?: return@withContext emptyList()
+            val block = getBlock(currentBlockHash) ?: return@runBlocking emptyList()
             resultArray.add(block)
             if (block.previousHash.isEmpty()) { // prevHash가 없으면 genesis block.
                 break
@@ -89,7 +89,7 @@ internal class BlockChainImpl(
             currentBlockHash = block.previousHash
         }
 
-        return@withContext resultArray
+        return@runBlocking resultArray
     }
 
     override fun getBlock(hash: String): Block? {
